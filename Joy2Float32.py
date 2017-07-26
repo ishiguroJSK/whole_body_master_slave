59383591e2db943fe467e2f0bf0017e73c1d404f
ishiguroJSK <ishiguroJSK@jsk.imi.i.u-tokyo.ac.jp>
2017-07-26 23:20:13 +0900

    add Joy2Float32

---
+++ Joy2Float32.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+import rospy
+from std_msgs.msg import Float32
+from sensor_msgs.msg import Joy
+
+pub = 0
+
+def cb(msg):
+#  sub_val_r_acp = msg.
+  pub.publish(Float32(msg.axes[2]))
+
+if __name__ == '__main__':
+  rospy.init_node('wbms_matplotlib_node', anonymous=True)
+  rospy.Subscriber("/joy_in", Joy, cb)
+  pub = rospy.Publisher("/float32_out", Float32, queue_size=10)
+  rospy.spin()
